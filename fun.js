c3cd902cf5c05455b6def5951496385562fb41b5
786basha <bashamasthan31@gmail.com>
2023-10-12 22:49:39 +0530

    first commitdd

---
+++ fun.js
@@ -0,0 +1,16 @@
+
+function haldi(){
+    window.location.href='https://drive.google.com/drive/folders/1PsJ_wkJzrBWmX-eGjhvI3V4uIczG7NOi?usp=drive_link';
+}
+
+function stills(){
+    window.location.href='https://drive.google.com/drive/folders/1RhA2C6FBGnt_OrcQqhRtEvjaJje51jpa?usp=drive_link';
+}
+
+function nalugu(){
+    window.location.href='https://drive.google.com/drive/folders/1QwguEhRu4kCAZ7ZV_MyqVvIEysn_XhTd?usp=drive_link';
+}
+
+function nikah(){
+    window.location.href='https://drive.google.com/drive/folders/1BlnAvSUdDGn403j6GBd3iiYcJm-0v_pF?usp=drive_link';
+}
